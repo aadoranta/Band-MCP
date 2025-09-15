8c356ae94d93ea80d41b0e9b5c39635aac0bfbc8
andrew-desktop <aadoranti@windesco.com>
2025-09-14 21:23:47 -0400

    Updated nomenclature

--- server.py
+++ server.py
@@ -6,18 +6,8 @@ mcp = FastMCP("bandmcp")
 SONGS_PATH = pathlib.Path("data/song_library.txt")
 
 
-@mcp.prompt()
-def purpose() -> str:
-    """Global instruction for test model"""
-    return f"""You are a virtual assistant for a band. Based on an input prompt, you will create
-    a setlist for a concert. You have access to a library of songs stored in {SONGS_PATH}.
-
-    REPLY WITH ONLY THE SETLIST, DO NOT REPEAT THE INSTRUCTIONS.
-    """
-
-
 @mcp.tool()
-def read_songs_library() -> str:
+def get_songs_library() -> str:
     """Location of all songs that we play together. This is the sole source for songs
     that should be arranged into a setlist."""
     with open(SONGS_PATH, "r") as file:
@@ -25,7 +15,7 @@ def read_songs_library() -> str:
 
 
 @mcp.tool()
-def read_email_from_subject_line(subject_line: str) -> dict:
+def get_email_from_subject_line(subject_line: str) -> dict:
     """
     subject_line: The subject line of the email to read
 
